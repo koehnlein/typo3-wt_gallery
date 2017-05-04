f44c3e1eecfc235c6445f7403ee5e08d367b89e3
Albrecht Köhnlein <ak@koehnlein.eu>
2017-05-04 16:56:27 +0200

    Remove loadTCA() function calls

--- ext_tables.php
+++ ext_tables.php
@@ -3,8 +3,6 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 $confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['wt_gallery']); // Get backandconfig
 t3lib_extMgm::addStaticFile($_EXTKEY, 'files/static/', 'Add default CSS');
 
-t3lib_div::loadTCA('tt_content');
-
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
 
